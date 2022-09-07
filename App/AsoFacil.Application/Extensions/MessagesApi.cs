15081c115ae198f1002b399eaa49d48ddbd1fac7
dslobo27 <dslobo27@gmail.com>
2022-09-07 19:57:18 -0300

    Adiciona extensões para tratamento das mensagens de erro e de rotas da api

---
+++ App/AsoFacil.Application/Extensions/MessagesApi.cs
@@ -0,0 +1,68 @@
+﻿namespace AsoFacil.Application.Extensions
+{
+    public static class MessagesApi
+    {
+        public static string Sucess(string entity, string action, EntityGender? entityGender = EntityGender.Masculino)
+        {
+            var actionMessage = GetAction(action, entityGender.Value);
+            return $"{entity} {actionMessage} com sucesso!";
+        }
+
+        public static string Error(string entity, string action, EntityGender? entityGender = EntityGender.Masculino)
+        {
+            var actionMessage = GetAction(action, entityGender.Value);
+            return $"{entity} não foi {actionMessage}. Tente novamente!";
+        }
+
+        public static string Exception(string entity, string action)
+        {
+            var actionMessage = GetActionOnException(action);
+            return $"Ocorreu um erro ao {actionMessage} {entity}!";
+        }
+
+        private static string GetArticle(EntityGender entityGender)
+        {
+            return entityGender.Equals(EntityGender.Masculino) ? "o" : "a";
+        }
+
+        private static string GetAction(string action, EntityGender entityGender)
+        {
+            var actionArr = action.Split("_");
+            switch (actionArr[0])
+            {
+                case "PUT":
+                    return string.Format("{0}{1}", "alterad", GetArticle(entityGender));
+
+                case "POST":
+                    return string.Format("{0}{1}", "criad", GetArticle(entityGender));
+
+                case "DELETE":
+                    return string.Format("{0}{1}", "excluid", GetArticle(entityGender));
+            }
+            return null;
+        }
+
+        private static string GetActionOnException(string action)
+        {
+            var actionArr = action.Split("_");
+            switch (actionArr[0])
+            {
+                case "PUT":
+                    return "alterar";
+
+                case "POST":
+                    return "criar";
+
+                case "DELETE":
+                    return "excluir";
+            }
+            return "obter";
+        }
+    }
+
+    public enum EntityGender
+    {
+        Masculino,
+        Feminino
+    }
+}
---
+++ App/AsoFacil.Application/Extensions/Routes.cs
@@ -0,0 +1,26 @@
+﻿namespace AsoFacil.Application.Extensions
+{
+    public static class Routes
+    {
+        #region Empresas
+
+        public const string POST_EMPRESAS = "api/empresas/v1/postasync";
+        public const string PUT_EMPRESAS = "api/empresas/v1/putasync";
+        public const string DELETE_EMPRESAS = "api/empresas/v1/deleteasync/{empresaId}";
+        public const string GET_EMPRESAS = "api/empresas/v1/getasync";
+        public const string GETBYID_EMPRESAS = "api/empresas/v1/getbyidasync/{empresaId}";
+
+        #endregion Empresas
+
+        #region Usuarios
+
+        public const string LOGIN = "api/usuarios/v1/loginasync";
+        public const string POST_USUARIOS = "api/usuarios/v1/postasync";
+        public const string PUT_USUARIOS = "api/usuarios/v1/putasync";
+        public const string DELETE_USUARIOS = "api/usuarios/v1/deleteasync/{usuarioId}";
+        public const string GET_USUARIOS = "api/usuarios/v1/getasync";
+        public const string GETBYID_USUARIOS = "api/usuarios/v1/getbyidasync/{usuarioId}";
+
+        #endregion Usuarios
+    }
+}
